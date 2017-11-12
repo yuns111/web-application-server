a2110189f9070ff40883424ac75ac3dcdb1aca73
AD01270148\USER <nt11381@nhn.com>
2017-11-12 16:05:32 +0900

    [3장] 요구사항1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,11 +28,26 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            String line = bufferedReader.readLine();
+
+            String[] tokens = line.split(" ");
+            String url = tokens[1];
+            byte[] body =  Files.readAllBytes(new File("./webapp" + url).toPath());
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
+
+            while(!"".equals(line)) {
+
+                line = bufferedReader.readLine();
+                if(line == null) {
+                    return;
+                }
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
