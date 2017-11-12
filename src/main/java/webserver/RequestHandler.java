376b0f373d04e4f0840116c568c24631fabdbccb
AD01270148\USER <nt11381@nhn.com>
2017-11-12 16:25:45 +0900

    [3장] 요구사항2

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,9 +10,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -34,14 +38,29 @@ public class RequestHandler extends Thread {
 
             String[] tokens = line.split(" ");
             String url = tokens[1];
-            byte[] body =  Files.readAllBytes(new File("./webapp" + url).toPath());
+            int index = url.indexOf("?");
+
+            String requestPath = getUrl(line);
+            Map<String, String> parsedParam = HttpRequestUtils.parseQueryString(line);
+
+            if(parsedParam != null) {
+                String id = parsedParam.get("userId");
+                String password = parsedParam.get("password");
+                String name = parsedParam.get("name");
+                String email = parsedParam.get("email");
+
+                User user = new User(id, password, name, email);
+
+                System.out.println(user.toString());
+            }
+
+            byte[] body =  Files.readAllBytes(new File("./webapp" + requestPath).toPath());
 
             DataOutputStream dos = new DataOutputStream(out);
             response200Header(dos, body.length);
             responseBody(dos, body);
 
             while(!"".equals(line)) {
-
                 line = bufferedReader.readLine();
                 if(line == null) {
                     return;
@@ -71,5 +90,35 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String getUrl(String line) {
+
+        String[] tokens = line.split(" ");
+        String url = tokens[1];
+        int index = url.indexOf("?");
+
+        String requestPath = url;
+
+        if(index != -1) {
+            requestPath = url.substring(0, index);
+        }
+
+        return requestPath;
+    }
+
+    private Map<String, String> getParams(String line) {
+
+        String[] tokens = line.split(" ");
+        String url = tokens[1];
+        int index = url.indexOf("?");
+
+        if(index == -1) {
+            return null;
+        }
+        String params = url.substring(index + 1);
+        Map<String, String> parsedParam = HttpRequestUtils.parseQueryString(params);
+
+        return parsedParam;
     }
 }
